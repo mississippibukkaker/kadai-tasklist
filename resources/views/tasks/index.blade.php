4e8ddd3abc3ec4075114c6feead62b5e3df5cd72
nyanchan <mktds13@outlook.jp>
2018-08-27 08:22:31 +0000

    resubmission

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,13 +7,12 @@
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
                  <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
                     
 
             @endforeach
-            {!! link_to_route('tasks.create', '新規タスクの作成') !!}
+            
         </ul>
     @endif
-
+    {!! link_to_route('tasks.create', '新規タスクの作成') !!}
 @endsection
